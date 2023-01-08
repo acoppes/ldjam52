54c5adc632952b27370bd1e890681f761b0e2b49
Ariel Coppes <ariel.coppes@gmail.com>
2023-01-07 23:21:49 -0300

    Just testing stuff with 2d camera

--- Assets/WormController.cs
+++ Assets/WormController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using MyBox;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,10 +12,20 @@ public class WormController : MonoBehaviour
 
     public float baseSpeed;
     public float maxSpeed;
+    public float minSpeed;
 
-    private float speed;
+    public float acceleration;
+    public float deceleration;
+    
+    [MyBox.ReadOnly]
+    public float speed;
 
+    [MyBox.ReadOnly]
     public float rotationSpeed;
+
+    public float baseRotationSpeed;
+
+    public float gravity;
 
     // Update is called once per frame
     void Update()
@@ -23,28 +35,52 @@ public class WormController : MonoBehaviour
         var dt = Time.deltaTime;
 
         var accelerate = playerInput.actions["Accelerate"];
+        var reverse = playerInput.actions["Reverse"];
+
         var turnRight = playerInput.actions["TurnRight"];
         var turnLeft = playerInput.actions["TurnLeft"];
 
-        var rotation = 0.0f;
+        var rotationDirection = 0.0f;
         
         if (accelerate.IsPressed())
         {
-            controller.Move(transform.forward * totalSpeed * dt);
-            // controller.SimpleMove(speed);
-        }    
+            speed += acceleration * dt;
+        }
+        else if (reverse.IsPressed())
+        {
+            speed -= acceleration * dt;
+        }
+        else
+        {
+            if (speed > 0)
+            {
+                speed -= deceleration * dt;
+            }
+            else
+            {
+                speed += deceleration * dt;
+            }
+        }
+
+        speed = Mathf.Clamp(speed, minSpeed, maxSpeed);
         
         if (turnLeft.IsPressed())
         {
-            rotation += 1f;
+            rotationDirection += 1f;
         }    
         
         if (turnRight.IsPressed())
         {
-            rotation += -1f;
-        }    
+            rotationDirection += -1f;
+        }
+
+        rotationSpeed = baseRotationSpeed + speed * 2;
         
-        transform.localEulerAngles += new Vector3(0, rotationSpeed * rotation * dt, 0);
-        controller.Move(transform.forward * totalSpeed * dt);
+        transform.localEulerAngles += new Vector3(0, rotationSpeed * rotationDirection * dt, 0);
+
+        var motion = transform.forward * totalSpeed * dt;
+        motion += new Vector3(0, gravity * dt, 0);
+
+        controller.Move(motion);
     }
 }
